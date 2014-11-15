2b58b1bc41c2f8e7285c25fe1e6f23b2d906de04
Mike Chu <mdc9919@me.com>
2014-11-14 22:38:53 -0500

    Initial Commit

---
+++ QuadCalc+/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  QuadCalc+
+//
+//  Created by Mike Chu on 11/14/14.
+//  Copyright (c) 2014 Mike Chu. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ QuadCalc+Tests/QuadCalc_Tests.swift
@@ -0,0 +1,36 @@
+//
+//  QuadCalc_Tests.swift
+//  QuadCalc+Tests
+//
+//  Created by Mike Chu on 11/14/14.
+//  Copyright (c) 2014 Mike Chu. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class QuadCalc_Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
